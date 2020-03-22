e9bacc51302d4c2d56713c38f6e366be311571fb
Dominique Cuevas <cuevasdominique@gmail.com>
2020-03-21 23:53:27 -0700

    Job & User tables, research TODO for datetime js/python conversion

--- backend/model.py
+++ backend/model.py
@@ -9,19 +9,48 @@ class Application(db.Model):
     __tablename__ = 'applications'
 
     application_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
-    # job_id = db. Column(db.Integer, db.ForeignKey('jobs.job_id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+    job_id = db. Column(db.Integer, db.ForeignKey('jobs.job_id'), nullable=True)
     datetime_applied = db.Column(db.DateTime, nullable=True)
-    referred_by = db.Column(db.Text, nullable=True)
+    referred_by = db.Column(db.VARCHAR(length=1000), nullable=True)
+    datetime_created = db.Column(db.DateTime, nullable=True)
+    # TODO: # react-chartjs-2
+    # using moment.js library, store as string w/ method .toString()
+    # let time = moment()
+    # time.toString() -> "Sat Mar 21 2020 20:37:27 GMT-0700"
+    # parse in python: date1 = datetime.strptime("Sat Mar 21 2020 20:37:27 GMT-0700", "%a %b %d %Y %X %Z%z")
+    # date1.strftime("%a %b %d %Y %X %Z%z") -> 'Sat Mar 21 2020 20:37:27 GMT-0700'
+    # use TIMESTAMP sqlalchemy type
+
+    user = db.relationship('User', backref='applications')
+    job = db.relationship('Job', backref='jobs')
+
+class Job(db.Model):
+    """Table of specific job posts"""
+
+    __tablename__ = 'jobs'
+
+    job_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    # company_id = db.Column(db.Integer, db.ForeignKey('companies.company_id'), nullable=True)
+    title = db.Column(db.VARCHAR(length=1000), nullable=False)
+    link = db.Column(db.VARCHAR(length=1000), nullable=False)
+    source = db.Column(db.VARCHAR(length=1000), nullable=False)
+    datetime_created = db.Column(db.DateTime, nullable=True)
+
+class User(db.Model):
+    """Table of users"""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.VARCHAR(length=1000), nullable=False)
+    password = db.Column(db.VARCHAR(length=20), nullable=False)
+    first_name = db.Column(db.VARCHAR, nullable=False)
+    last_name = db.Column(db.VARCHAR, nullable = False)
+    points_total = db.Column(db.Integer, nullable=False, default=0)
     datetime_created = db.Column(db.DateTime, nullable=True)
 
 def seed():
-    # TODO: use javascript Date().getTime()
-    # future scope using chartjs use momentjs library
-        # now = moment() for client's locale time
-        # change format now.format('x') for timestamp in milliseconds
-        # or now.format('YYYY M D')
-    # use TIMESTAMP sqlalchemy type
     datetime_applied, datetime_created = datetime.now(), datetime.now()
     application = Application(datetime_applied=datetime_applied, referred_by="Anjelica", datetime_created=datetime_created)
     db.session.add(application)
