dd4ca561b13631e85ed5a8cbfdfe65852bcdf285
Dominique Cuevas <cuevasdominique@gmail.com>
2020-04-01 22:08:02 -0700

    useEffect dependency array

--- frontend/src/components/Applications.js
+++ frontend/src/components/Applications.js
@@ -1,31 +1,31 @@
 import React from "react"
-import {useState, useEffect} from "react"
+import {useState, useEffect, useRef} from "react"
 
-function Applications() {
+function Applications(props) {
     const [applications, setApplications] = useState([]);
+    const latestProps = useRef(props);
     useEffect(() => {
+        latestProps.current = props;
         async function fetchUser() {
-            console.log('in useEffect');
             const result = await fetch('/user');
             const user = await result.json();
             const applications_array = user.applications;
-            console.log(applications_array);
             setApplications(
                 applications_array.map(function(application) {
                     return (
                         <details key={application.application_id}>
                             <summary>{application.job.title} - {application.job.company.company_name}</summary>
                             <p>Applied: {Date(application.datetime_applied)}</p>
+                            <p>Current Status: {application.application_statuses[0].status}</p>
                             <details>
-                                <summary>{application.application_statuses[0].datetime_created} - {application.application_statuses[0].status}</summary>
-                                {application.application_statuses.length > 1 && 
-                                application.application_statuses.slice(1).map((application_status) => 
+                                <summary>Status History</summary>
+                                {application.application_statuses.map((application_status) => 
                                 <p key={application_status.application_status_id}>{application_status.datetime_created} - {application_status.status}</p>)}
                             </details>
+                            <p>Source: {application.job.source}</p>
                             <p>Link: <a 
                                 href={application.job.link}
                                 target='_blank'>{application.job.link}</a></p>
-                            <p>Source: {application.job.source}</p>
                             <p>Referred by: {application.referred_by}</p>
                         </details>
                     )
@@ -34,7 +34,8 @@ function Applications() {
             )
         }
         fetchUser();
-    }, []);
+    }, [props]);
+    // TODO: stale state on change of Applications
 
     return (
         <div>
