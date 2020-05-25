149a54ad3468c10b01618c61c2e69a4c8a10fb98
Dominique Cuevas <cuevasdominique@gmail.com>
2020-05-24 17:39:23 -0700

    passed test for rendering application form

---
+++ frontend/src/components/ApplicationForm.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import ApplicationForm from "./ApplicationForm";
+
+let container = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+})
+
+it("renders", () => {
+    act(() => {
+        render(<ApplicationForm 
+            handleChange={() => {}}
+            handleSubmit={() => {}} 
+            inputs={{
+                title: '',
+                link: '',
+                source: '',
+                company_name: '',
+                website: '',
+                status: '',
+            }} 
+            setInput={() => {}}
+            initialInputs={{
+                title: '',
+                link: '',
+                source: '',
+                company_name: '',
+                website: '',
+                status: '',
+            }}
+        />, container);
+    });
+    expect(container.textContent).toContain("Enter new application:");
+
+})
