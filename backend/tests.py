c4667c24935f21adfb508ab68f230a29adeea456
Dominique Cuevas <cuevasdominique@gmail.com>
2020-07-20 23:15:31 -0700

    test new applications and application statuses

--- backend/tests.py
+++ backend/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from server import app
-from model import connect_to_db, db, seed, seed_types, User, Company, Job, Application
+from model import connect_to_db, db, seed, seed_types, User, Company, Job, Application, ApplicationStatus
 
 class FlaskTestsDatabase(TestCase):
 
@@ -70,11 +70,11 @@ class FlaskTestsDatabase(TestCase):
         )
         self.client.post('/new-application',
             data={'company_name': 'Second Company',
-                'website': 'https://www.secondcompany.com',
-                'title': 'Second Title',
-                'link': 'https://www.secondlink.com',
-                'source': 'LinkedIn',
-                'status': 'Applied'
+                  'website': 'https://www.secondcompany.com',
+                  'title': 'Second Title',
+                  'link': 'https://www.secondlink.com',
+                  'source': 'LinkedIn',
+                  'status': 'Applied'
             }
         )
         result = db.session.query(Application).all()
@@ -84,80 +84,35 @@ class FlaskTestsDatabase(TestCase):
         result = db.session.query(Job).all()
         self.assertEqual(len(result), 3)
 
+    def test_status_change(self):
+        self.client.post('/new-application',
+            data={'company_name': 'Company Name',
+                  'website': 'https://www.company.com',
+                  'title': 'Title',
+                  'link': 'https://www.link.com',
+                  'source': 'LinkedIn',
+                  'status': 'Applied'
+            }
+        )
+        application = db.session.query(Application).first()
+        
+        result = self.client.post('/new-application-status',
+            data={
+                'application_id': application.application_id,
+                'new_status': 'Phone interviewed'
+            }
+        )
+        self.assertEqual(result.status_code, 200)
 
-    # def test_application(self):
-    #     result = self.client.get('/application/1')
-    #     self.assertIn(b'application_statuses', result.data)
-    
-    # def test_application_status(self):
-    #     result = self.client.get('/application_status/1')
-    #     self.assertIn(b'Applied', result.data)
+        application = db.session.query(Application).first()
+        application_statuses = application \
+            .application_statuses \
+            .order_by(ApplicationStatus.datetime_created.desc()) \
+            .all()
+        
+        self.assertEqual(application_statuses[0].status, 'Phone interviewed')
+        self.assertEqual(application_statuses[1].status, 'Applied')
 
-    # def test_journal_entry(self):
-    #     result = self.client.get('/journal_entry/2')
-    #     self.assertIn(b'Another journal entry', result.data)
-
-    # def test_job(self):
-    #     result = self.client.get('/job/1')
-    #     self.assertIn(b'www.linkedin.com/mycompany/software-engineer', result.data)
-    
-    # def test_company(self):
-    #     result = self.client.get('company/1')
-    #     self.assertIn(b'My Company', result.data)
-
-    # def test_point_entries(self):
-    #     result = self.client.get('/point_entries')
-    #     self.assertIn(b'/point_entry_type/2', result.data)
-
-    # def test_point_entry_type(self):
-    #     result = self.client.get('/point_entry_type/2')
-    #     self.assertIn(b'uas', result.data)
-    
-    # def test_badges(self):
-    #     result = self.client.get('/badges')
-    #     self.assertIn(b'100 points', result.data)
-
-    # def test_badge_type(self):
-    #     result = self.client.get('/badge_type/1')
-    #     self.assertIn(b'100 points', result.data)
-
-    # def test_post_application(self):
-    #     # new application with existing company and new job
-    #     result = self.client.post('/new-application', 
-    #         data={'company_name': 'My Company',
-    #             'website': 'https://www.mycompany.com',
-    #             'title': 'Fullstack Engineer',
-    #             'link': 'https://glassdoor.com/newcompany/fullstackengineer',
-    #             'source': 'Glassdoor',
-    #             'status': 'Applied',
-    #         },
-    #         follow_redirects=True
-    #     )
-    #     self.assertIn(b'My Company', result.data)
-    #     num_companies = len(Company.query.all())
-    #     self.assertEqual(num_companies, 2)  # 2 existing companies from seed()
-
-    #     # new application with existing company and job
-    #     result = self.client.post('/new-application', 
-    #         data={'company_name': 'Another Company',
-    #             'website': 'https://www.anothercompany.com',
-    #             'title': 'Software Engineer',
-    #             'link': 'https://www.linkedin.com/anothercompany/software-engineer',
-    #             'source': 'LinkedIn',
-    #             'status': 'Applied',
-    #         },
-    #         follow_redirects=True
-    #     )
-    #     self.assertIn(b'Another Company', result.data)
-    #     num_jobs = len(Job.query.all())
-    #     self.assertEqual(num_jobs, 4)  # 2 existing application from seed() + 2 more tests
-
-    # def test_point_entry_on_application_status_change(self):
-    #     result = self.client.post('/new-application-status',
-    #         data={'new_status': 'Applied',
-    #         'application_id': 1}
-    #     )
-    #     self.assertIn(b'uas', result.data)
 
 if __name__ == "__main__":
     import unittest
